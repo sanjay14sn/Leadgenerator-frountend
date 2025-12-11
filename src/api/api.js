72ff9d0fa8c28750e5ebca25e64e07ac67db5332
SANJAY NAVEEN <sanjaynaveen@SANJAYs-MacBook-Air.local>
2025-12-11 21:14:56 +0530

    test1

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "http://localhost:5007/api",
-// });
 const API = axios.create({
-  baseURL: "https://leadgenerator-backend-production.up.railway.app/api",
+  baseURL: "http://localhost:5009/api",
 });
+// const API = axios.create({
+//   baseURL: "https://leadgenerator-backend-production.up.railway.app/api",
+// });
 
 export default API;
