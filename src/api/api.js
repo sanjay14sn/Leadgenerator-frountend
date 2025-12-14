8aeb4af8f7b238c9915504730e6d9dac3ff88fe9
SANJAY NAVEEN <sanjaynaveen@SANJAYs-MacBook-Air.local>
2025-12-14 14:35:59 +0530

    base url change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5010/api",
+  // baseURL: "http://localhost:5010/api",
+  baseURL:"leadgenerator-backend-production.up.railway.app",
   timeout: 15000,
 });
 
